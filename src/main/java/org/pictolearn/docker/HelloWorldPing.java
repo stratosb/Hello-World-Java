4c670f01da47605a2f6aa64d4f4ce40dd5fc0c55
stratosb <polydefkis12@gmail.com>
2020-10-29 16:01:54 +0200

    Hi from Stratos

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hi World Ping: " + i );
+			System.out.println("Ping from Stratos " + i );
 			Thread.sleep(1000);
 		}
 	}
